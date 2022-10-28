1cfb93ce3180d6f321b53193e21860b70fb0681a
antoniochangoer <54006126+antoniochangoer@users.noreply.github.com>
2022-10-28 20:35:02 +0200

    client validation

--- js/app.js
+++ js/app.js
@@ -4,34 +4,32 @@ const lastName = document.querySelector('#lastname');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const form = document.querySelector('.form');
-const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    resetFields();
-    
-    for (const field of inputFields) {
-        if(field.value === '' || field.value === null) {
-            field.classList.add('warning');
-            field.parentElement.classList.add('error');
-            field.nextElementSibling.classList.add('warning-msg');
-        } else {
-            checkMailformat();
-        }
-    }
+const checkRequired = (fields) => {
+    fields.forEach( field => {
+      if (field.value.trim() === '') {
+        toggleError(field);
+      } else {
+        console.log('success ' + field.name);
+      }
+    });
+  }
 
-});
 
-const checkMailformat = () => {
-    console.log(emailFormat.test(email.value));
-    if (emailFormat.test(email.value)) {
-        resetFields();
+const checkMailformat = (e) => {
+    const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (emailFormat.test(e.value.trim())) {
+        console.log('good email')
     } 
     else {
-        email.classList.add('warning');
-        email.parentElement.classList.add('error');
-        email.nextElementSibling.classList.add('warning-msg');
+        toggleError(email);
     }
+}
+
+const toggleError = (el) => {
+    el.classList.add('warning');
+    el.parentElement.classList.add('error');
+    el.nextElementSibling.classList.add('warning-msg');
 }
 
 const resetFields = () => {
@@ -41,3 +39,10 @@ const resetFields = () => {
         field.nextElementSibling.classList.remove('warning-msg');
     }
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    resetFields();
+    checkRequired(inputFields);
+    checkMailformat(email);
+});
